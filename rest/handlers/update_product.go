6d938b229ff03d7e2d7511869207c9c06208b29b
Mamun172136 <ma1536204@gmail.com>
2025-09-21 13:51:44 +0600

    feat:add update product

---
+++ rest/handlers/update_product.go
@@ -0,0 +1,30 @@
+package handlers
+
+import (
+	"ecommerce/database"
+	"ecommerce/util"
+	"encoding/json"
+	"net/http"
+	"strconv"
+)
+
+func UpdateProduct(w http.ResponseWriter, r *http.Request){
+	productId := r.PathValue("id")
+	pId,err := strconv.Atoi(productId)
+	if err != nil{
+		http.Error(w, "please give me a valid product id",400)
+		return
+	}
+
+	var newProduct database.Product
+	decoder := json.NewDecoder(r.Body)
+	err = decoder.Decode(&newProduct)
+	if err != nil{
+		http.Error(w, "please give me valid json",400)
+		return
+	}
+	
+	newProduct.ID = pId
+	database.Update(newProduct)
+	util.SendData(w,"successfully updated product", 201)
+}
--- rest/routes.go
+++ rest/routes.go
@@ -8,8 +8,9 @@ import (
 
 func initRoutes(mux *http.ServeMux, manager *middleware.Manager){
 	mux.Handle("/hello", manager.With(http.HandlerFunc(handlers.Test)))
-	mux.Handle("/products", manager.With(http.HandlerFunc(handlers.GetProducts)))
-
-	mux.Handle("/create-products", manager.With(http.HandlerFunc(handlers.CreateProducts)))
+	mux.Handle("GET /products", manager.With(http.HandlerFunc(handlers.GetProducts)))
+	mux.Handle("GET /products/{id}", manager.With(http.HandlerFunc(handlers.GetProductById)))
+	mux.Handle("PUT /products/{id}", manager.With(http.HandlerFunc(handlers.GetProductById)))
+	mux.Handle("POST /create-products", manager.With(http.HandlerFunc(handlers.CreateProducts)))
 
 }
