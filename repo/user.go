92a336b91de4cbee03d559a0e663792a99422cb9
Mamun172136 <ma1536204@gmail.com>
2025-10-02 11:41:30 +0600

    feat(user): add Create method to insert user into database

--- repo/user.go
+++ repo/user.go
@@ -1,5 +1,7 @@
 package repo
 
+import "github.com/jmoiron/sqlx"
+
 type User struct {
 	ID          int    `json:"Id"`
 	FirstName   string `json:"first_name"`
@@ -9,36 +11,54 @@ type User struct {
 	IsShopOwner bool   `json:"is_shop_owner"`
 }
 
-type UserRepo interface{
-	Create(user User)(*User, error)
-	Find(email,password string)(*User,error)
+type UserRepo interface {
+	Create(user User) (*User, error)
+	Find(email, password string) (*User, error)
 }
 
-type userRepo struct{
-	users []User
+type userRepo struct {
+	// users []User
+	db *sqlx.DB
 }
 
-func NewUserRepo()UserRepo{
+func NewUserRepo(db *sqlx.DB) UserRepo {
 	return &userRepo{
-
+		db: db,
 	}
 }
 
-func (r userRepo)Create(user User)(*User, error){
-		if user.ID != 0 {
-		return &user,nil
+func (r userRepo) Create(user User) (*User, error) {
+	// 	if user.ID != 0 {
+	// 	return &user,nil
+	// }
+	// user.ID = len(r.users) + 1
+	// r.users = append(r.users, user)
+	// return &user,nil
+
+	query := `
+		INSERT INTO users (first_name, last_name, email, password, is_shop_owner)
+		VALUES (:first_name, :last_name, :email, :password, :is_shop_owner)
+		RETURNING id
+	`
+	var userID int
+	rows,err:=  r.db.NamedQuery(query,user )
+	if err != nil{
+		return nil, err
 	}
-	user.ID = len(r.users) + 1
-	r.users = append(r.users, user)
-	return &user,nil
+
+	if rows.Next(){
+		rows.Scan(&userID)
+	}
+	user.ID = userID
+	return &user,err
 }
 
-func (r userRepo)Find(email, password string)(*User,error){
-	for _, u := range r.users {
-		if u.Email == email && u.Password == password {
-			return &u, nil
-		}
-	}
-	return nil,nil
-}
+func (r userRepo) Find(email, password string) (*User, error) {
+	// for _, u := range r.users {
+	// 	if u.Email == email && u.Password == password {
+	// 		return &u, nil
+	// 	}
+	// }
+	 return nil, nil
 
+}
